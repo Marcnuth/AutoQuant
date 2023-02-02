4461928adce5775a645ae6be1fae579311aeb507
Marcnuth <hxianxian@qq.com>
2023-02-02 16:29:20 +0800

    release 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="AutoQuant",
-    version="1.0.0",
+    version="1.0.1",
     author="NAUTIDEA",
     author_email="xian@nautidea.com",
     description="Auto Quant",
